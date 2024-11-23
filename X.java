c4a4b87c891e9053f3aad1ecd2abd3e3839b5c02
• 🫀 ⁾⁾﹕ 𝐌 𝐢 𝐫 𝐢 ⌇ <160249239+1tsm1ri@users.noreply.github.com>
2024-11-22 22:05:48 -0500

    Update X.java

--- X.java
+++ X.java
@@ -2,7 +2,7 @@ public class X {
 
     public static void main(String[] args) {
         
-        System.out.println("Esto es un mensaje");
+        System.out.println("Hello World");
     }
     
 }
